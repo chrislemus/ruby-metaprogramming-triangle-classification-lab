0a00fb351134421900d336e39d97f735859a735e
chrislemus <icrislemus@gmail.com>
2020-12-28 12:42:35 -0500

    commit tree e10f597c33b145bd20dc487b5c188933e1e1610f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_reader :kind
+  def initialize(side1, side2, side3)
+    sides = [side1, side2, side3]
+    sides.each{|side|
+      no_side = side <= 0
+      triangle_inequality = (sides.inject(0, :+) - side) > side
+      raise TriangleError if !triangle_inequality || no_side
+    }
+    case sides.uniq.length
+    when 1
+      @kind = :equilateral
+    when 2
+      @kind = :isosceles
+    when 3
+      @kind = :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
 end
